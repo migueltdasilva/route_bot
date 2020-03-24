2596e6a4cbf4026a34ccb773c0d32e103cf83ef5
Nikita <nsalekhin@gmail.com>
2020-03-25 02:25:31 +0300

    14 commit

--- src/main/java/RouteBot.java
+++ src/main/java/RouteBot.java
@@ -1,8 +1,14 @@
 
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.Audio;
+import org.telegram.telegrambots.meta.api.objects.Document;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
@@ -14,9 +20,12 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static java.lang.System.out;
 
 public class RouteBot extends Bot {
 
@@ -28,6 +37,27 @@ public class RouteBot extends Bot {
         super(name, token);
         hmChat2Answers = new HashMap<>();
         hmChat2UserInfo = new HashMap<>();
+    }
+
+    private enum Command {
+        COMMAND_LIST("/", "Список всех команд."),
+        START("/start", "Начать все сначала."),
+        SEND_JOKE("/send_joke", "Могу отправить тебе шутку.");
+
+        String name;
+        String desr;
+        public static Map<String, Command> cmdByName;
+        public static Command byName(String name) {
+            return cmdByName.get(name);
+        }
+        Command(String name, String desr) {
+            this.name = name;
+        }
+        static {
+            for(Command cmd : Command.values()) {
+                cmdByName.put(cmd.name, cmd);
+            }
+        }
     }
 
     private static final String[] vTrips = new String[]{"Хочу в road trip по Киргизии"};
@@ -48,9 +78,8 @@ public class RouteBot extends Bot {
     private Map<Long, String> hmChat2UserInfo;
 
 
-    public synchronized void setTripButtons(SendMessage sendMessage) {
+    public synchronized ReplyKeyboardMarkup getTripButtons() {
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
         replyKeyboardMarkup.setOneTimeKeyboard(false);
@@ -62,6 +91,8 @@ public class RouteBot extends Bot {
             alKeyboardRows.add(keyboardFirstRow);
         }
         replyKeyboardMarkup.setKeyboard(alKeyboardRows);
+
+        return replyKeyboardMarkup;
     }
 
     private void setInline() {
@@ -74,15 +105,19 @@ public class RouteBot extends Bot {
         markupKeyboard.setKeyboard(buttons);
     }
 
+    @Override
+    public synchronized void sendMsg(String chatId, String s) {
 
-    public synchronized void sendMsg(String chatId, String s, boolean bBtn) {
+        sendMsg(chatId, s, null);
+    }
+
+    public synchronized void sendMsg(
+            String chatId, String s, ReplyKeyboardMarkup replyKeyboardMarkup) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(chatId);
         sendMessage.setText(s);
-        if (bBtn) {
-            setTripButtons(sendMessage);
-        }
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
@@ -92,55 +127,167 @@ public class RouteBot extends Bot {
     }
 
 
+    public synchronized void sendAudio(String chatId, String fileId) {
+        SendAudio audio = new SendAudio();
+        audio.setChatId(chatId);
+        audio.setAudio(fileId);
+        try {
+            execute(audio);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+            //Log.log(Level, "Exception: ", e.toString());
+        }
+    }
+
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            Message msg = update.getMessage();
-            Long chatId = msg.getChatId();
 
-            String message = msg.getText();
-            System.out.println("LOG: onUpdateReceived: msg = " + message);
+            handleTextMsg(update);
+        } else if (update.hasMessage() && update.getMessage().hasAudio()) {
 
-            if (message.equals("/start")) {
-                System.out.println("LOG: onUpdateReceived: deleting answers");
-                hmChat2Answers.put(chatId, new ArrayList<>());
+            out.println("LOG: onUpdateReceived: audio msg");
+            handleAudioMsg(update);
+        } else if (update.hasMessage() && update.getMessage().hasPhoto()) {
 
-                sendMsg(
-                        chatId.toString(),
-                        "Привет! Тут можно записаться в поездку рута ⚡️", true);
+            out.println("LOG: onUpdateReceived: photo msg");
+            handlePhotoMsg(update);
+        }  else if (update.hasMessage() && update.getMessage().hasDocument()) {
 
-                return;
-            }
+            out.println("LOG: onUpdateReceived: document msg");
+            handleDocMsg(update);
+        }
+    }
+
+    private void handleTextMsg(Update update) {
+        Message msg = update.getMessage();
+        Long chatId = msg.getChatId();
+
+        String message = msg.getText();
+        out.println("LOG: onUpdateReceived: msg = " + message);
+
+        if (message.startsWith("/")) {
+            out.println("LOG: onUpdateReceived: cmd recieved");
+            handleCmd(update);
+
+            return;
+        }
+
+        User usr = msg.getFrom();
+        String userName = "";
+        if (usr != null) {
+            userName = usr.getUserName();
+        }
+
+        List<String> alAns =
+                hmChat2Answers.getOrDefault(chatId, new ArrayList<>());
+
+        out.println("LOG: onUpdateReceived: alAns = [" + alAns.stream().reduce("", (s, s2) -> s + " " + s2) + "]");
+        out.println("LOG: onUpdateReceived: has user = [" + hmChat2UserInfo.get(chatId) + "]");
+
+        String msgText = "";
+        String chooseOpt = "0";
+        ReplyKeyboardMarkup rkM = null;
+        if (alAns.size() == 0 && hmChat2UserInfo.get(chatId) == null) {
+            chooseOpt = "1";
+            msgText = vQuestions[0];
+            hmChat2UserInfo.put(chatId, userName);
+            rkM = getTripButtons();
+        } else if (alAns.size() == 6) {
+            //TODO сделать нормальную отправук
+            chooseOpt = "2";
+            sendMsg(adminChatId, alAns.stream().reduce("", (s, s2) -> s+ "\n" + s2));
+            msgText = "Кайф, спасибо! Передам Коле и Алине все ответы, они свяжутся с тобой в ближайшее время. Если хочешь начать заново, нажми сюда /start";
+        } else {
+            chooseOpt = "3";
+            alAns.add(message);
+            msgText = vQuestions[alAns.size()];
+        }
+        out.println("LOG: onUpdateReceived: msg text = [" + msgText + "] $ " + chooseOpt);
+
+        sendMsg(chatId.toString(), msgText, rkM);
+    }
+
+    private void handleCmd(Update update) {
+        Message msg = update.getMessage();
+        String message = msg.getText();
+        Long chatId = msg.getChatId();
+        Command cmd = Command.byName(message.toUpperCase());
+        if (cmd == Command.START) {
+            out.println("LOG: onUpdateReceived: deleting answers");
+            hmChat2Answers.put(chatId, new ArrayList<>());
+            hmChat2UserInfo.put(chatId, null);
 
 
-            User usr = msg.getFrom();
-            String userName = "";
-            if (usr != null) {
-                userName = usr.getUserName();
-            }
+            sendMsg(
+                    chatId.toString(),
+                    "Привет! Тут можно записаться в поездку рута ⚡️", getTripButtons());
 
-            List<String> alAns =
-                    hmChat2Answers.getOrDefault(chatId, new ArrayList<>());
-            System.out.println("LOG: onUpdateReceived: alAns = [" + alAns.stream().reduce("", (s, s2) -> s + " " + s2) + "]");
-            System.out.println("LOG: onUpdateReceived: has user = [" + hmChat2UserInfo.get(chatId) + "]");
-            String msgText = "";
-            String chooseOpt = "0";
-            if (alAns.size() == 0 && hmChat2UserInfo.get(chatId) == null) {
-                chooseOpt = "1";
-                msgText = vQuestions[0];
-                hmChat2UserInfo.put(chatId, userName);
-            } else if (alAns.size() == 6) {
-                //TODO
-                chooseOpt = "2";
-                sendMsg(adminChatId, alAns.stream().reduce("", (s, s2) -> s+ "\n" + s2));
-                msgText = "Кайф, спасибо! Передам Коле и Алине все ответы, они свяжутся с тобой в ближайшее время. Если хочешь начать заново, нажми сюда /start";
-            } else {
-                chooseOpt = "3";
-                alAns.add(message);
-                msgText = vQuestions[alAns.size()];
-            }
-            System.out.println("LOG: onUpdateReceived: msg text = [" + msgText + "] $ " + chooseOpt);
+        } else if (cmd == Command.COMMAND_LIST) {
 
-            sendMsg(chatId.toString(), msgText, false);
+        } else if (cmd == Command.SEND_JOKE) {
+            sendMsg(String.valueOf(chatId),"Шутка - хуютка!");
+        } else {
+            sendMsg(String.valueOf(chatId),
+                    "Простите, я не знаю такой команды, я пенс. Вот команды, которые я знаю:");
+
+        }
+    }
+    private void handleAudioMsg(Update update) {
+        Audio audio = update.getMessage().getAudio();
+        String fileId = audio.getFileId();
+        SendAudio msg = new SendAudio();
+        msg.setAudio(fileId);
+        msg.setChatId(adminChatId);
+        msg.setCaption("Вот ваш файл " + fileId + " " +
+                audio.getFileUniqueId() +  " " +
+                audio.getMimeType() + " " +
+                audio.getPerformer() + " " +
+                audio.getTitle() );
+
+        try {
+            execute(msg);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handlePhotoMsg(Update update) {
+        List<PhotoSize> photos = update.getMessage().getPhoto();
+
+        String fileId = photos.stream()
+                .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                .findFirst()
+                .orElse(null).getFileId();
+
+        String caption = photos.stream().map(photoSize -> photoSize.toString()).reduce("", String::concat);
+        SendPhoto msg = new SendPhoto();
+        msg.setPhoto(fileId);
+        msg.setChatId(adminChatId);
+        msg.setCaption("Вот ваш файл: " + caption);
+
+        try {
+            execute(msg);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleDocMsg(Update update) {
+
+        Document doc = update.getMessage().getDocument();
+
+        String fileId = doc.getFileId();
+
+        String caption = doc.toString();
+        SendDocument msg = new SendDocument();
+        msg.setDocument(fileId);
+        msg.setChatId(adminChatId);
+        msg.setCaption("Вот ваш файл: " + caption);
+
+        try {
+            execute(msg);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
@@ -155,4 +302,6 @@ public class RouteBot extends Bot {
             e.printStackTrace();
         }
     }
+
 }
+
