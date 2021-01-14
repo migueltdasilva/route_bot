1c3594fb3657d10cb1479c2069d3a977528be850
Nikita <nsalekhin@gmail.com>
2021-01-14 16:39:04 +0300

    mailing 2

--- src/main/java/RouteBot.java
+++ src/main/java/RouteBot.java
@@ -830,7 +830,9 @@ public class RouteBot extends Bot {
         int i = 0;
         for (String chatId : hsUsers) {
             Long chat = Long.parseLong(chatId.substring(1));
-            String userName = getUserName(chat);
+            String userName = getUserName(chat) + " " +
+                jedis.get("a" + chatId + "_" + 1) + " " +
+                jedis.get("a" + chatId + "_" + 2);
             chat = adminChatId;
             try {
                 if (fileId == null) {
