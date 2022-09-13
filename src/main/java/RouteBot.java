afce3fd91ba5d1a868a6b1289ad5dcd73ccc289d
Nikita <nsalekhin@gmail.com>
2022-09-13 19:19:51 +0300

    zel 2

--- src/main/java/RouteBot.java
+++ src/main/java/RouteBot.java
@@ -1091,7 +1091,7 @@ public class RouteBot extends Bot {
                     String.valueOf(chatId),
                     "Привет\\! Тут можно записаться в поездку рута ⚡️ \n" +
                         "Сейчас есть варианты:" +
-                        "\n - [Дальние-дальние Зеленцы 30 сентября \\- 2 октября](https://t.me/routecommunity/527) \n" +
+                        "\n - [Дальние\\-дальние Зеленцы 30 сентября \\- 2 октября](https://t.me/routecommunity/527) \n" +
                         "\n - Киргизия 3\\-9 октября",
                     getInlineKeyBoardWithTrips(), true);
             }
